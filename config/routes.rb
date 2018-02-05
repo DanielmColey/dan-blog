926f174ee5f9b8244aaa471eeddddf43633fa5b4
DanielmColey <dmcoley948562@gmail.com>
2018-02-05 14:00:42 -0700

    added blog scaffold and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/home'
+  root to: "blogs#index"
+
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

2620a67e0a6c9f7a983688e3abcd623c9a17e209
DanielmColey <dmcoley948562@gmail.com>
2018-02-06 09:42:37 -0700

    added devise styles and authorizations

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.paginate(:page => params[:page], :per_page => 10)
   end
 
   # GET /blogs/1
